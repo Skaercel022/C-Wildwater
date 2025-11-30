c762db80b7c3056a3d83f96cc5eb095a85ac4e2f
Skaercel022 <tom01.nerson@gmail.com>
2025-11-30 22:27:28 +0100

    implantation verif arguments

---
+++ bash/c-wildwater.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#verifie qu'il y'ait au moins un argument
+if [ $# -lt 1 ]; then
+    echo "Erreur : aucune commande fournie."
+    echo "4 Usage possibles :"
+    echo "Usage 1 : $0 histo max"
+    echo "Usage 2 : $0 histo src"
+    echo "Usage 3 : $0 histo real"
+    echo "Usage 4 : $0 leaks "ID_USINE""
+    exit 1
+fi
+
+cmd="$1"
+
+#verif commandes histo
+if [ "$cmd" -eq "histo" ]; then
+    if [ $# -ne 2 ]; then
+        echo "Erreur : usage -> $0 histo {max|src|real}"
+        exit 1
+    fi
+
+    mode="$2"
+
+    if [ "$mode" -ne "max" ] && [ "$mode" -ne "src" ] && [ "$mode" -ne "real" ]; then
+        echo "Erreur : mode histo invalide : $mode"
+        echo "Modes valides : max | src | real"
+        exit 1
+    fi
+
+#verif commandes leaks
+elif [ "$cmd" -eq "leaks" ]; then
+
+    if [ $# -ne 2 ]; then
+        echo "Erreur : usage -> $0 leaks "ID_USINE""
+        exit 1
+    fi
+
+    usine="$2"
+
+#commandes inconnues
+else
+    echo "Erreur : commande inconnue '$cmd'"
+    echo "Commandes valides : histo | leaks"
+    exit 1
+fi
