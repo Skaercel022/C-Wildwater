466b84731a80aef182f792e7ab82684e4d6b489e
Skaercel022 <tom01.nerson@gmail.com>
2025-12-04 11:15:12 +0100

    fonctions avl

--- codeC/src/avl.c
+++ codeC/src/avl.c
@@ -1,5 +1,37 @@
 #include "../include/include.h"
 
+AVL* creerAVL(int value) {
+    AVL* pAVL = malloc(sizeof(AVL));
+    if (pAVL == NULL) {
+        exit(1);
+    }
+    pAVL->val = value;
+    pAVL->pGauche = NULL;
+    pAVL->pDroit = NULL;
+    pAVL->equilibre = 0;
+    return pAVL;
+}
+
+int existeGauche(AVL* pAVL) {
+    if (pAVL == NULL) {
+        exit(2);
+    }
+    if (pAVL->pGauche != NULL) {
+        return 1;
+    }
+    return 0;
+}
+
+int existDroit(AVL* pAVL) {
+    if (pAVL == NULL) {
+        exit(2);
+    }
+    if (pAVL->pDroit != NULL) {
+        return 1;
+    }
+    return 0;
+}
+
 AVL* rotationDroite(AVL* pAVL) {
     //rotation
     AVL* pivot = pAVL->pGauche;
@@ -62,5 +94,97 @@ AVL* equilibrerAVL(AVL* pAVL) {
     return pAVL;
 }
 
+AVL* insertionAVL(AVL* pAVL, int e, int* h) {
+    if (pAVL == NULL) {
+        *h = 1;
+        return creerAVL(e);
+    }
+    else if (pAVL->val > e) {
+        pAVL->pGauche = insertionAVL(pAVL->pGauche, e, h);
+        *h = - *h;
+    }
+    else if (pAVL->val < e) {
+        pAVL->pDroit = insertionAVL(pAVL->pDroit, e, h);
+    }
+    else {
+        *h = 0;
+        return pAVL;
+    }
 
+    if (*h != 0) {
+        pAVL->equilibre += *h;
+        pAVL = equilibrerAVL(pAVL);
+        if (pAVL->equilibre == 0) {
+            *h = 0;
+        }
+        else {
+            *h = 1;
+        }
+    }
+    return pAVL;
+}
+
+AVL* suppMinAVL(AVL* pAVL, int* pe, int *h) {
+    AVL* tmp = NULL;
+    if (pAVL->pGauche == NULL) {
+        *pe = pAVL->val;
+        *h = 1;
+        tmp = pAVL;
+        pAVL = pAVL->pDroit;
+        free(tmp);
+        return pAVL;
+    }
+    else {
+        pAVL->pGauche = suppMinAVL(pAVL->pGauche, pe , h);
+        *h = - *h;
+    }
+    if (*h != 0) {
+        pAVL->equilibre += *h;
+        pAVL = equilibrerAVL(pAVL);
+        if (pAVL->equilibre == 0) {
+            *h = 0;
+        }
+        else {
+            *h = 1;
+        }
+    }
+    return pAVL;
+}
+
+AVL* suppressionAVL(AVL* pAVL, int e, int* h) {
+    AVL* tmp = NULL;
+    if (pAVL == NULL) {
+        *h = 0;
+        return pAVL;
+    }
+    else if (pAVL->val < e) {
+        pAVL->pDroit = suppressionAVL(pAVL->pDroit, e, h);
+    }
+    else if (pAVL->val > e) {
+        pAVL->pGauche = suppressionAVL(pAVL->pGauche, e, h);
+        *h = - *h;
+    }
+    else if (existDroit(pAVL)) {
+        pAVL->pDroit = suppMinAVL(pAVL->pDroit, h, &e);
+    }
+    else {
+        tmp = pAVL;
+        pAVL = pAVL->pGauche;
+        free(tmp);
+        *h = -1;
+        return pAVL;
+    }
+
+    if (*h != 0) {
+        pAVL->equilibre += *h;
+        pAVL = equilibrerAVL(pAVL);
+        if (pAVL->equilibre == 0) {
+            *h = -1;
+        }
+        else {
+            *h = 0;
+        }
+    }
+    return pAVL;
+}
 
