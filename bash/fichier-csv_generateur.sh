5b0ea7c937bbb049b0813e7d0d580998e80e0bd4
bilel-z <bill.zaroui@gmail.com>
2025-12-05 17:49:51 +0100

    Update fichier-csv_generateur.sh

--- bash/fichier-csv_generateur.sh
+++ bash/fichier-csv_generateur.sh
@@ -7,3 +7,18 @@ fi
     
 if [ "$mode" = "src" ]; then
 	echo "Usine,prelevement">> vol_capation.txt"
+
+if [ ! -f "$fichier" ] ; then 
+	echo "Erreur fichier introuvable:$fichier"
+	exit 1
+ fi  
+fichier="$1"
+case "$mode" in  
+	max)
+		echo "Usine,traitementmax,source,prelevement,">>vol_max.dat
+   	 	val=$( grep -E '^-;[^-;]+;-;' $ficher | cut -d ';' -f2,4)
+    		echo "$val" >>valmax.cs
+    		;;
+    	src)
+    		echo 
+grep -E '^-;[^-;]+;' c-wildwater_v0.dat | cut -d ';' -f2,4,5
