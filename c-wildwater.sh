e05bb29bd37dff29736dcf4156fd3e21e65be97b
bilel-z <bill.zaroui@gmail.com>
2025-12-13 16:18:35 +0100

    Update c-wildwater.sh

--- c-wildwater.sh
+++ c-wildwater.sh
@@ -11,52 +11,189 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-cmd="$1"
 
-#verif commandes histo
-if [ "$cmd" = "histo" ]; then
-    if [ $# -ne 2 ]; then
-        echo "Erreur : usage -> $0 histo max|src|real"
-        exit 1
-    fi
+#
 
-    mode="$2"
+#!/bin/bash
 
-    if [ "$mode" != "max" ] && [ "$mode" != "src" ] && [ "$mode" != "real" ]; then
-        echo "Erreur : mode histo invalide : $mode"
-        echo "Modes valides : max | src | real"
-        exit 1
-    fi
 
-#verif commandes leaks
-elif [ "$cmd" = "leaks" ]; then
 
-    if [ $# -ne 2 ]; then
-        echo "Erreur : usage -> $0 leaks "ID_USINE""
-        exit 1
-    fi
 
-    usine="$2"
 
-#commandes inconnues
-else
-    echo "Erreur : commande inconnue '$cmd'"
-    echo "Commandes valides : histo | leaks"
+# Vérification fichier
+
+fichier="$1"
+
+cmd="$2"
+
+mode="$3"
+
+
+
+if [ ! -f "$fichier" ]; then 
+
+    echo "Erreur : fichier introuvable : $fichier"
+
     exit 1
-fi
 
-#compilation et verif compilation
+fi  
 
-run="run"
+case "$cmd" in
 
-cd "$(dirname "$0")/codeC"
+	histo)
 
-make
+		case "$mode" in  
 
-if [ -f "$run" ]; then
-    ./"$run"
-    make clean
-else
-    echo "Erreur make"
-    exit 2
-fi
+
+
+    			max)
+
+        			echo "Usine;TraitementMax" > vol_max.dat
+
+        			val=$( grep -E '^-;[^-;]+;-;' c-wildwater_v0.dat | cut -d ';' -f2,4)
+
+    				echo "$val" > vol_max.dat
+
+        			echo "Fichiers généré : vol_max.dat"
+
+					input_files="vol_max.dat"
+
+        			;;
+
+
+
+    			src)
+
+				echo "Usine/source;TraitementMax;Fuite" > source.csv
+
+        			val=$(grep -E '^-;[^-;]+;' $fichier | cut -d ';' -f2,4,5 )
+
+        			echo "$val" > source.csv
+
+        			echo "Fichier généré : source.csv"
+
+					input_files="source.csv"
+
+       				;;
+
+
+
+    			prelevement)
+
+				echo "Usine/source;TraitementMax;Fuite" > prelevement.csv
+
+        			val=$(grep -E '^-;[^-;]+;' $fichier | cut -d ';' -f2,4,5 )
+
+        			echo "$val" > prelevement.csv
+
+        			echo "Fichier généré : prelevement.csv"
+
+					input_files="prelevement.csv"
+
+        			;;
+
+
+
+    			*)
+
+        			echo "Mode inconnu : $mode"
+
+        			echo "Modes disponibles : max / source / prelevement"
+
+    				exit 1
+
+        			;;
+
+		esac
+
+	
+
+		esac
+
+
+
+	 	Compilation et appel du programme C 
+
+        if [ -f Makefile ]; then
+
+            echo "Compilation du programme C..."
+
+            make
+
+        else
+
+            echo "Makefile introuvable. erreur"
+
+			exit(1)
+
+        fi
+
+		
+
+		if [ ! -x "./mon_programme_C" ]; then
+
+            echo "Erreur : l'exécutable du programme C n'existe pas."
+
+            exit 1
+
+        fi
+
+
+
+        echo "Exécution du programme C pour le mode $mode..."
+
+        ./mon_programme_C "$mode" $input_files
+
+
+
+        echo "Traitement terminé pour le mode $mode."
+
+        ;;
+
+	
+
+	leaks)
+
+		echo "Identifiant;TraitementMax;fuite" > fuite.csv
+
+		val=$(grep -E "ID_USINE" $fichier | cut -d ';' -f2,4,5 )
+
+		echo "$val">fuite.csv
+
+		echo "Fichier généré: fuite.csv"
+
+		;;
+
+	*)
+
+	echo "commande inconnu : $cmd"
+
+    	echo "commandes disponibles : histo / leaks"
+
+    	exit 1
+
+	;;
+
+esac
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
